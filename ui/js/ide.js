5c3437dc1fe113ba76ac875f7ab49381f7b2a10b
esuwu <sanya554455@gmail.com>
2022-10-02 09:06:33 -0500

    Added the first version

---
+++ ui/js/ide.js
@@ -0,0 +1,26 @@
+let editor;
+
+window.onload = function() {
+    editor = ace.edit("editor");
+    editor.setTheme("ace/theme/monokai");
+
+}
+
+function changeCompiler() {
+    let compiler = $("#compiler").val();
+
+    if (compiler == "GowavesCompiler") {
+       // send request to gowaves compiler
+    }
+
+    if (compiler == "WavesCompiler") {
+        // send request to waves compiler
+    }
+
+}
+
+function executeCode() {
+    $.sjax({
+        url: "/execute",
+    })
+}
